c86288c93c42ec0440172c5b6b581205c12a1470
Max Tyskevich <2842778@gmail.com>
2021-03-15 09:36:45 +0300

    feat: task towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    let m = matrix.slice(); // copy array, so that dont correct array matrix
+    let result = [];
+
+    m.forEach((str, index) => {
+        if (index % 2) {
+            str.reverse();
+        }
+        str.forEach((val) => result.push(val));
+    });
+
+    return result;
+};
